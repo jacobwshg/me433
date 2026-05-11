606e96fd24af11902f3cc8e38718010067b969ae
jacobwshg <wsg438@gmail.com>
2026-05-11 17:21:32 -0500

    [hw2-pwm] implemented angle GPIO level caching to avoid recomputation

--- hw2-pwm_____/pico_pwm.c
+++ hw2-pwm_____/pico_pwm.c
@@ -4,6 +4,8 @@
 #include "hardware/pwm.h" // CMakeLists.txt must have hardware_pwm in target_link_libraries
 #include "hardware/adc.h" // CMakeLists.txt must have hardware_adc in target_link_libraries
 
+typedef int angle_t;
+
 static const uint PWM_PIN = 14;
 
 // 
@@ -12,8 +14,17 @@ static const uint PWM_PIN = 14;
 static const float DIV = 150.0F; // must be between 1-255
 static const uint16_t WRAP = 20000; // when to rollover, must be less than 65535
 
-typedef int angle_t;
+// 
+// cache mapping from angles to GPIO levels
+// to avoid recomputing on the fly
+//
+static uint64_t ANGLE_LV_TBL[ 200 ];
 
+static const angle_t
+	MINANGLE = 10,
+	MAXANGLE = 170;
+
+/*
 bool
 timer_interrupt_function( __unused struct repeating_timer *t )
 {
@@ -23,20 +34,17 @@ timer_interrupt_function( __unused struct repeating_timer *t )
 	printf( "%f\r\n",( float )result1 / 4095 * 3.3 );
 	return true;
 }
+*/
 
-uint16_t
-deg_to_gpio_level( const float deg )
+static int
+init_angle_lv_tbl( void )
 {
 	static const float INV_180 = 1.0F / 180.0F;
 
-	//
-	// fraction of pulse duty cycle within PWM freq
-	//
 	static const float
 		// conservative
 		//MIN_DUTY_FRAC = 0.05F,
 		//MAX_DUTY_FRAC = 0.10F;
-
 		MIN_DUTY_FRAC = 0.025F,
 		MAX_DUTY_FRAC = 0.125F;
 
@@ -44,48 +52,48 @@ deg_to_gpio_level( const float deg )
 		MIN_DUTY = WRAP * MIN_DUTY_FRAC,
 		MAX_DUTY = WRAP * MAX_DUTY_FRAC;
 
-	const float duty_delta = 
-		( deg * INV_180 ) * ( MAX_DUTY-MIN_DUTY );
+	for ( angle_t angle=0; angle<=180; ++angle )
+	{
+		const float duty_delta = 
+		( ( float ) angle * INV_180 ) * ( MAX_DUTY-MIN_DUTY );
    
-	const uint16_t lv = ( uint16_t )
+		const uint16_t lv = ( uint16_t )
 		( MIN_DUTY + duty_delta );
 
-	return lv;
+		ANGLE_LV_TBL[ angle ] = lv;
+	}
+	return 0;
 }
 
 static inline void
-setangle( const angle_t angle )
+setangle( const angle_t angle_ )
 {
+	angle_t angle = angle_;
+	if ( angle > MAXANGLE ) { angle = MAXANGLE; }
+	if ( angle < MINANGLE ) { angle = MINANGLE; }
 	pwm_set_gpio_level(
 		PWM_PIN,
-		deg_to_gpio_level( ( float ) angle )
+		ANGLE_LV_TBL[ angle ]
 	);
 }
 
 int 
 main()
 {
-	static const angle_t
-		MINANGLE = 10,
-		MAXANGLE = 170;
+	init_angle_lv_tbl();
 
 	static const uint32_t SLEEP_DUR = 10;
 
 	stdio_init_all();
 
 	// turn on a timer interrupt
-	struct repeating_timer timer;
+	//struct repeating_timer timer;
 	// -100 means call the function every 100ms
 	// +100 would mean call the function 100ms after the function has ended
 	//add_repeating_timer_ms( -100, timer_interrupt_function, NULL, &timer );
 
 	gpio_set_function( PWM_PIN, GPIO_FUNC_PWM ); // Set the Pin to be PWM
 	const uint slice_num = pwm_gpio_to_slice_num( PWM_PIN ); // Get PWM slice number
-
-	//pwm_config pwm_cfg = pwm_get_default_config();
-	//pwm_config_set_clkdiv( &pwm_cfg, DIV ); // sets the clock speed
-	//pwm_config_set_wrap  ( &pwm_cfg, WRAP ); 
-	//pwm_init( slice_num, &pwm_cfg, true );
 
 	pwm_set_clkdiv( slice_num, DIV );
 	pwm_set_wrap( slice_num, WRAP );
