9b964206c63feefdff74b9bccff64c2a9c64a796
jacobwshg <wsg438@gmail.com>
2026-05-21 05:01:56 -0500

    [hw3-i2c-dip] button can control LED but behavior is inverted

--- hw3-i2c-dip/main.c
+++ hw3-i2c-dip/main.c
@@ -12,24 +12,27 @@
 
 #define HEARTBEAT_PIN 20
 
-enum MCPOpcode 
+#define MCP_LED_OUT_PIN 7
+#define MCP_BTN_IN_PIN  0
+
+enum MCPRegId
 {
-    IODIR   = 0x00,
-    IPOL    = 0x01,
-    GPINTEN = 0x02,
-    DEFVAL  = 0x03,
-    INTCON  = 0x04,
-    IOCON   = 0x05,
-    GPPU    = 0x06,
-    INTF    = 0x07,
-    INTCAP  = 0x08,
-    GPIO    = 0x09,
-    OLAT    = 0x0a,
+    MCP_IODIR   = 0x00,
+    MCP_IPOL    = 0x01,
+    MCP_GPINTEN = 0x02,
+    MCP_DEFVAL  = 0x03,
+    MCP_INTCON  = 0x04,
+    MCP_IOCON   = 0x05,
+    MCP_GPPU    = 0x06,
+    MCP_INTF    = 0x07,
+    MCP_INTCAP  = 0x08,
+    MCP_GPIO    = 0x09,
+    MCP_OLAT    = 0x0a,
 };
 
-typedef uint8_t mcp_addr_t; // only 7 bits are used
-
-static const mcp_addr_t MCP_ADDR_DEFAULT = 0x20; // 010'0xxx
+#define MCP_I2C_ADDR_6_3 0x20 // 0b010'0xxx
+#define MCP_I2C_ADDR_2_0 0x07 // custom: 0b111
+#define MCP_I2C_ADDR ( MCP_I2C_ADDR_6_3 | MCP_I2C_ADDR_2_0 )
 
 static void
 heartbeat_init( void )
@@ -80,6 +83,58 @@ set_heartbeat( void )
 
 }
 
+void
+write_i2c_device(
+    const uint8_t addr,
+    const uint8_t reg, const uint8_t val
+)
+{
+    bool nostop;
+    uint8_t buf[ 2 ];
+    buf[ 0 ] = reg; buf[ 1 ] = val;
+    i2c_write_blocking(
+        I2C_PORT,
+        addr,
+        buf, 2,
+        nostop=false
+    );
+}
+
+uint8_t
+read_i2c_device(
+    const uint8_t addr, 
+    const uint8_t reg
+)
+{
+    uint8_t buf;
+    bool nostop;
+    i2c_write_blocking(
+        I2C_PORT,
+        addr,
+        &reg, 1,
+        nostop=true
+    );
+    i2c_read_blocking(
+        I2C_PORT,
+        addr,
+        &buf, 1,
+        nostop=false
+    );
+    return buf;
+}
+
+
+static void
+mcp_init( void )
+{
+    uint8_t mcp_iodir_val = 0x0;
+    // register MCP button input pin
+    mcp_iodir_val |= ( 0x1 << MCP_BTN_IN_PIN );
+    write_i2c_device(
+        MCP_I2C_ADDR, MCP_IODIR, mcp_iodir_val
+    );
+}
+
 int
 main()
 {
@@ -94,15 +149,32 @@ main()
     
     gpio_set_function( I2C_SDA_PIN, GPIO_FUNC_I2C );
     gpio_set_function( I2C_SCL_PIN, GPIO_FUNC_I2C );
-    gpio_pull_up( I2C_SDA_PIN );
-    gpio_pull_up( I2C_SCL_PIN );
+    //gpio_pull_up( I2C_SDA_PIN );
+    //gpio_pull_up( I2C_SCL_PIN );
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    mcp_init();
 
     while ( true )
     {
         set_heartbeat();
-        //printf( "Hello, world!\n" );
-        //sleep_ms( 1000 );
+
+        const uint8_t mcp_gpio_val = read_i2c_device(
+            MCP_I2C_ADDR,
+            MCP_GPIO
+        );
+        const uint8_t btn_val = 0x1 & ( mcp_gpio_val >> MCP_BTN_IN_PIN );
+
+        uint8_t mcp_olat_val = 0x0;
+        if ( btn_val )
+        {
+            mcp_olat_val |= ( 0x1 << MCP_LED_OUT_PIN );
+        }
+        write_i2c_device(
+            MCP_I2C_ADDR,
+            MCP_OLAT, mcp_olat_val
+        );
+
     }
 
 }
