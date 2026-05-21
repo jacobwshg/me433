790f30c2b163851556c36649e55367195b192045
jacobwshg <wsg438@gmail.com>
2026-05-21 11:45:15 -0500

    [hw4-i2c-oled] setup

---
+++ hw4-i2c-oled/main.cpp
@@ -0,0 +1,40 @@
+
+//#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include <cstdio>
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT i2c0
+//#define I2C_SDA 8
+//#define I2C_SCL 9
+
+namespace Pins
+{
+    static constexpr uint
+        I2C_SDA_PIN { 4 },
+        I2C_SCL_PIN { 5 }
+    ;
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init( I2C_PORT, 400*1000 );
+    
+    gpio_set_function( Pins::I2C_SDA_PIN, GPIO_FUNC_I2C );
+    gpio_set_function( Pins::I2C_SCL_PIN, GPIO_FUNC_I2C );
+    gpio_pull_up( Pins::I2C_SDA_PIN );
+    gpio_pull_up( Pins::I2C_SCL_PIN );
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    while ( true )
+    {
+        std::printf( "Hello, world!\n" );
+        sleep_ms( 1000 );
+    }
+}
