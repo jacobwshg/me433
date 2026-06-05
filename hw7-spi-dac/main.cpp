0e5c6c83b3c2cd2dbec29f3ef3a51527ee699f38
jacobwshg <wsg438@gmail.com>
2026-06-05 09:41:59 -0500

    [hw7-spi-dac] set 16-bit SPI format; fixed init_Vout_cache() increment without assigning to element 0; verified values of digital Vout with serial output

--- hw7-spi-dac/main.cpp
+++ hw7-spi-dac/main.cpp
@@ -17,9 +17,7 @@ namespace Pins
 
 namespace
 {
-    static constexpr std::uint32_t BAUD_MHZ { 1 };
-    static constexpr std::uint32_t KHZ_PER_MHZ { 1000 };
-    static constexpr std::uint32_t BAUD_KHZ { BAUD_MHZ * KHZ_PER_MHZ };
+    static constexpr std::uint32_t BAUD_KHZ { 12 };
     static constexpr std::uint32_t BAUD_HZ { BAUD_KHZ * 1000 };
 
     // sine and triangle wave frequencies
@@ -56,23 +54,26 @@ init_Vout_cache( void )
     float rad { 0.0F };
     for ( std::uint16_t &Vout : ( ::SIN_PERIOD_VOUT_CACHE ) )
     {
-        rad += ::SIN_SAMPLE_DRAD;
         float sin_val { std::sin( rad ) };
         // map from [ -1, 1 ] to [ 0, 1 ]
         sin_val = 0.5F + ( 0.5F * sin_val ); 
         Vout = MCP4912::Vout_from_scale( sin_val );
+
+        rad += ::SIN_SAMPLE_DRAD;
     }
 
     float y { 0.0F };
     std::size_t idx { 0 };
     for ( std::uint16_t &Vout : ( ::TRI_PERIOD_VOUT_CACHE ) )
     {
+        Vout = MCP4912::Vout_from_scale( y );
+
         if ( idx < TRI_SAMPLES_PER_PERIOD / 2 )
         {
             y += ::TRI_SAMPLE_DY_ABS;
         }
         else { y -= ::TRI_SAMPLE_DY_ABS; }
-        Vout = MCP4912::Vout_from_scale( y );
+
         ++idx;
     }
 }
@@ -84,36 +85,45 @@ main()
 
     init_Vout_cache();
 
-    //spi_set_format( SPI_PORT, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST );
-
     // SPI initialisation. This example will use SPI at 1MHz.
     spi_init( SPI_PORT, ::BAUD_HZ );
-    gpio_set_function( Pins::SPI_MISO, GPIO_FUNC_SPI );
-    gpio_set_function( Pins::SPI_CS,   GPIO_FUNC_SIO );
+    spi_set_format( SPI_PORT, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST );
+
+    //gpio_set_function( Pins::SPI_MISO, GPIO_FUNC_SPI );
+    //gpio_set_function( Pins::SPI_CS,   GPIO_FUNC_SIO );
     gpio_set_function( Pins::SPI_SCK,  GPIO_FUNC_SPI );
     gpio_set_function( Pins::SPI_MOSI, GPIO_FUNC_SPI );
-    
+
     // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_init( Pins::SPI_CS );
     gpio_set_dir( Pins::SPI_CS, GPIO_OUT );
     gpio_put( Pins::SPI_CS, 1 );
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
-    std::size_t i_sin { 0 }, i_tri { 0 };
+    MCP4912::write( MCP4912::Channel::A, MCP4912::Vout_from_f( 3.3F ) );
+    MCP4912::write( MCP4912::Channel::B, MCP4912::Vout_from_f( 3.3F ) );
+    sleep_ms( 100 );
 
-    // MCP4912::write( MCP4912::Channel::A, MCP4912::Vout_from_f( 0.0F ) );
-    // MCP4912::write( MCP4912::Channel::B, MCP4912::Vout_from_f( 1.0F ) );
+    std::size_t i_sin { 0 }, i_tri { 0 };
 
     while ( true )
     {
         const absolute_time_t now { get_absolute_time() };
 
-        MCP4912::write( MCP4912::Channel::A, ::SIN_PERIOD_VOUT_CACHE[ i_sin ] );
-        MCP4912::write( MCP4912::Channel::B, ::TRI_PERIOD_VOUT_CACHE[ i_tri ] );
-        // MCP4912::write( MCP4912::Channel::A, static_cast<std::uint16_t>( 0U ) ); // test
-        // MCP4912::write( MCP4912::Channel::B, MCP4912::DMAX ); // test
+        const std::uint16_t
+            Vout_sin { ::SIN_PERIOD_VOUT_CACHE[ i_sin ] },
+            Vout_tri { ::TRI_PERIOD_VOUT_CACHE[ i_tri ] };
+
+        std::printf( "sin: %04u, tri: %04u\n", Vout_sin, Vout_tri );
+
+        MCP4912::write( MCP4912::Channel::A, Vout_sin );
+        MCP4912::write( MCP4912::Channel::B, Vout_tri );
+
         if ( ++i_sin == SIN_SAMPLES_PER_PERIOD ) { i_sin = 0; }
         if ( ++i_tri == TRI_SAMPLES_PER_PERIOD ) { i_tri = 0; }
-        sleep_until( now + ::SLEEP_UNTIL_DT );
+        //sleep_until( now + ::SLEEP_UNTIL_DT );
+
+        sleep_ms( 5 );
 
     }
 }
--- hw7-spi-dac/mcp4912.h
+++ hw7-spi-dac/mcp4912.h
@@ -64,18 +64,25 @@ MCP4912::make_command( const Channel chan )
 static inline void
 MCP4912::write(  const Channel chan, const std::uint16_t data )
 {
-    const std::uint16_t pkt
+    std::uint16_t pkt
     {
         make_command( chan )
         | ( ( data & DATA_MSK ) << DATA_OFS )
     };
     
-    static std::array< std::uint8_t, 2 > buf {};
-    buf[ 0 ] = static_cast<std::uint8_t>( pkt >> 8 );
-    buf[ 1 ] = static_cast<std::uint8_t>( pkt & 0xffU );
+    // static std::array< std::uint8_t, 2 > buf {};
+    // buf[ 0 ] = static_cast<std::uint8_t>( pkt >> 8 );
+    // buf[ 1 ] = static_cast<std::uint8_t>( pkt & 0xffU );
 
+    // SPIUtil::cs_select();
+    // spi_write16_blocking( SPI_PORT, buf.data(), 1 );
+    // SPIUtil::cs_deselect();
+
+    //pkt = __builtin_bswap16( pkt );
     SPIUtil::cs_select();
-    spi_write_blocking( SPI_PORT, buf.data(), buf.size() );
+    sleep_us( 1 );
+    spi_write16_blocking( SPI_PORT, &pkt, 1 );
+    sleep_us( 1 );
     SPIUtil::cs_deselect();
 }
 
