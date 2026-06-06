485d569647dcf33b38161bb79ef979fbcfb6b9de
jacobwshg <wsg438@gmail.com>
2026-06-05 20:15:56 -0500

    [hw9-dsp] added IIR

--- hw9-dsp/filter.py
+++ hw9-dsp/filter.py
@@ -1,6 +1,17 @@
 
 from collections import deque
 
+def clamp( val, mn, mx ):
+	assert( mn <= mx )
+	if val > mx:
+		val = mx
+	if val < mn:
+		val = mn
+	return val
+
+"""
+Moving average filter
+"""
 class MAF:
 
 	def __init__( self, tap=10 ):
@@ -23,4 +34,30 @@ class MAF:
 	def get_avg( self ):
 		return sum( self.buf ) / self.tap
 
+"""
+Infinite impulse response filter
+"""
+class IIR:
+
+	def __init__( self, a=0.5 ):
+		self.a = clamp( a, 0.0, 1.0 )
+		self.b = 1.0 - self.a
+		self.avg = 0.0
+
+	def add_sample( self, sample ):
+		sample = float( sample )
+		self.avg = self.a * self.avg + self.b * sample
+
+	def add_samples( self, sample_lst ):
+		for smp in sample_lst:
+			self.add_sample( sample )
+
+	def get_avg( self ):
+		return self.avg
+
+"""
+Finite impulse response filter
+"""
+class FIR:
+	pass
 
