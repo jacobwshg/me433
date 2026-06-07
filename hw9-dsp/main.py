caab7e48c3974bc63034eb36e626ec289951938e
jacobwshg <wsg438@gmail.com>
2026-06-07 09:42:44 -0500

    [hw9-dsp] added original, MAF and IIR pIIR plots

--- hw9-dsp/main.py
+++ hw9-dsp/main.py
@@ -20,7 +20,7 @@ def plot_fd( ts, ds, xlabel="freq", ylabel="ampl", title="" ):
 	plt.show()
 
 # plot amplitudes in both domains 
-def plot_td_fd( ts, ds_td, freqs, ds_fd, savname="", title="" ):
+def plot_td_fd( ts, ds_td, freqs, ds_fd, figname="", title="" ):
 	fig, ( ax1, ax2 ) = plt.subplots( 2, 1 )
 	ax1.plot( ts, ds_td, "b" )
 	ax1.set_xlabel( "time" )
@@ -29,14 +29,14 @@ def plot_td_fd( ts, ds_td, freqs, ds_fd, savname="", title="" ):
 	ax2.set_xlabel( "freq" )
 	ax2.set_ylabel( "|ampl|" )
 	plt.suptitle( title )
-	plt.savefig( savname )
+	plt.savefig( figname )
 	plt.show()
 
 # plot both raw and filtered amplitudes in both domains 
 def plot_td_fd_cmp(
 	ts, raw_td, filtered_td,
 	freqs, raw_fd, filtered_fd,
-	title=""
+	figname="", title=""
 ):
 	fig, ( ax1, ax2 ) = plt.subplots( 2, 1 )
 	ax1.plot( ts, raw_td, "black" )
@@ -48,10 +48,14 @@ def plot_td_fd_cmp(
 	ax2.set_xlabel( "freq" )
 	ax2.set_ylabel( "|ampl|" )
 	plt.suptitle( title )
+	plt.savefig( figname )
 	plt.show()
 
 
-def run_maf( cnt, data, ts, fft_out, tap=64 ):
+def run_maf(
+	cnt, data, ts, fft_out, tap=64,
+	figname="maf.png", title=""
+):
 	maf = filter.MAF( tap=32 )
 
 	maf_out = [ 0.0 ] * cnt
@@ -69,11 +73,14 @@ def run_maf( cnt, data, ts, fft_out, tap=64 ):
 	plot_td_fd_cmp(
 		ts, data, maf_out,
 		freqs, fft_out, maf_fft_out,
-		title=f"MAF tap={ maf.tap }"
+		figname=figname, title=title
 	)
 
 
-def run_iir( cnt, data, ts, A=0.999 ):
+def run_iir(
+	cnt, data, ts, A=0.999,
+	figname="iir.png", title=""
+):
 	iir = filter.IIR( A=A )
 
 	iir_out = [ 0.0 ] * cnt
@@ -89,11 +96,14 @@ def run_iir( cnt, data, ts, A=0.999 ):
 	plot_td_fd_cmp(
 		ts, data, iir_out,
 		freqs, fft_out, iir_fft_out,
-		title=f"IIR A={ iir.A }"
+		figname=figname, title=title
 	)
 
 
-def run_fir( cnt, data, ts, coefs ):
+def run_fir(
+	cnt, data, ts, coefs,
+	figname="fir.png", title=""
+):
 	fir = filter.FIR( coefs=coefs )
 
 	fir_out = [ 0.0 ] * cnt
@@ -106,7 +116,26 @@ def run_fir( cnt, data, ts, coefs ):
 	fft_out = fft_out[ range( cnt // 2 ) ]
 	freqs = freqs[ range( cnt // 2 ) ]
 
-	plot_td_fd( ts, data, freqs, fft_out )
+	plot_td_fd(
+		ts, data, freqs, fft_out,
+		figname=figname, title=title
+	)
+
+MAF_TAPS = \
+{
+	"A": 128,
+	"B": 128,
+	"C": 2,
+	"D": 96
+}
+
+IIR_A = \
+{
+	"A": 0.998,
+	"B": 0.995,
+	"C": 0.5,
+	"D": 0.95
+}
 
 # sampling rate 400Hz, cutoff 10Hz, transition BW 50Hz
 coefs_D = [
@@ -167,10 +196,18 @@ if __name__ == "__main__":
 
 	#plot_loglog( freqs, fft_out )
 
-	savname = f"{ sig_name }.png"
+	figname = f"{ sig_name }.png"
 	title = sig_name + f" (Fs={ int( sampling_rate ) }Hz)"
-	plot_td_fd( ts, data, freqs, fft_out, savname=savname, title=title )
+	#plot_td_fd( ts, data, freqs, fft_out, figname=figname, title=title )
 
-	#run_maf( cnt, data, ts, fft_out, tap=64 )
-	#run_iir( cnt, data, ts, A=0.995 )
+	maf_tap = MAF_TAPS[ tag ]
+	maf_figname = f"{ sig_name }_maf.png"
+	maf_title=f"{ sig_name } MAF ( tap={ maf_tap } )"
+	maf_title
+	#run_maf( cnt, data, ts, fft_out, tap=maf_tap, figname=maf_figname, title=maf_title )
+
+	iir_A = IIR_A[ tag ]
+	iir_figname = f"{ sig_name }_iir.png"
+	iir_title=f"{ sig_name } IIR ( A={ iir_A } )"
+	run_iir( cnt, data, ts, A=iir_A, figname=iir_figname, title=iir_title )
 
